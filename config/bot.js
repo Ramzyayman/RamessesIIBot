8a568a184f620cefe385d831c75999dbd41be687
Ramzyayman <36305487+Ramzyayman@users.noreply.github.com>
2021-02-04 15:54:53 +0200

    Create bot.js

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'NzM3MzE4ODU3OTQwNzk1NDM0.Xx7nsg.Ngpbgw6NuHMCBvIwMrWdNL-_YvY',
+        prefix: '-',
+        activity: 'developed by Ramesses.',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
